624e49ece98379e0c216790a9a669d8a8905f147
Ryan Baumgardner <rbaumgardner@planview.com>
2022-02-03 14:41:55 -0500

    Complete merge-sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,39 @@
   You'll need to write more than just one function
 */
 
+const merge = ( left, right ) => {
+	const results = [];
+
+	while( left.length && right.length ) {
+		if ( left[0] <= right[0] ) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
+
+	return results.concat( left, right );
+}
+
 const mergeSort = (nums) => {
-  // code goes here
+	if ( nums.length < 2 ) {
+		return nums;
+	}
+
+	const length = nums.length;
+	const middle = Math.floor( length / 2 );
+	const left = nums.slice( 0, middle );
+	const right = nums.slice( middle );
+
+	const sortedLeft = mergeSort( left );
+	const sortedRight = mergeSort( right );
+
+	return merge( sortedLeft, sortedRight );
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
