79110163d81ed8d973fdfd15ed95588fb9e584a5
Ryan Baumgardner <rbaumgardner@planview.com>
2022-02-11 15:02:45 -0500

    Complete quick sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -1,9 +1,9 @@
 /*
 
   Quick Sort!
-  
+
   Name your function quickSort.
-  
+
   Quick sort should grab a pivot from the end and then separate the list (not including the pivot)
   into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
   lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
@@ -13,12 +13,37 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+	// base case, array of length 0 or 1
+	if ( nums.length <= 1 ) {
+		return nums;
+	}
+
+	// choose pivot
+	const pivot = nums[ nums.length - 1 ];
+
+	// separate into left and right arrays
+	let left = [];
+	let right = [];
+
+	for ( let i = 0; i < nums.length - 1; i++ ) {
+		if ( nums[i] < pivot ) {
+			left.push(nums[i]);
+		} else {
+			right.push(nums[i]);
+		}
+	}
+
+	// call quickSort on left and right arrays
+
+	let sortedLeft = quickSort(left);
+	let sortedRight = quickSort(right);
+
+	return sortedLeft.concat( pivot, sortedRight )
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
